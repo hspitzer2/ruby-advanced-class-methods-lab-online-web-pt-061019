323991d77b5f93a54bf34abf1df1ab140b0c2ce9
Hillary Spitzer <hspitzer2@gmail.com>
2019-07-15 15:25:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,6 +52,7 @@ class Song
    def self.new_from_filename(name)
      song = self.new
      song.name = (name.split(" - ")[1].chomp(".mp3"))
+     binding.pry
      song.artist_name = (name.split(" - ")[0])
      song
      #shoud return `Taylor Swift - Blank Space.mp3`
